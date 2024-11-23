db34c5a34fb560e4562dbb8104016853c01a168b
smartersig <50397668+smartersig@users.noreply.github.com>
2024-11-23 08:54:29 +0000

    Update MySportsAIfree.py

--- MySportsAIfree.py
+++ MySportsAIfree.py
@@ -35,9 +35,6 @@ def predModel():
       for col in decs.columns:
         fileName = fileName + col[0:2]
       fileName = fileName + '.sav'
-      with inputs:
-        with rescol:
-          st.write (fileName)
 
       try:
         with open(fileName, "rb") as f:
@@ -45,10 +42,7 @@ def predModel():
           for col in cols:
             decs[col] = decs[col].fillna(m.repNaNs[col])
       except Exception as e:
-        with inputs:
-          with rescol:
-            st.write (e)
-        #print ('EXCEPTION ',e)
+        print ('EXCEPTION ',e)
 
       preds = m.predict_proba(decs)
       preds = preds[:,1:]
